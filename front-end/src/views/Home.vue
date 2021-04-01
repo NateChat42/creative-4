ea94fbe12f84d83a9de943ba6cd240a9104f6b9a
Nathan Chatwin <nathanchatwin@gmail.com>
2021-04-01 12:14:01 -0600

    got characters showing properly; TODO make two pages and stuff

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,19 +1,21 @@
 <template>
 <div>
-  <h1>Things To Do</h1>
-  <div id="projects">
-    <button :style="{backgroundColor: project.color}" :class="{ white: darkColor(project.color), selected: active(project)}" v-for="project in projects" :key=project.id @click=selectProject(project)>{{project.name}}</button>
+  <h1>Characters</h1>
+  <div id="characters">
+    <button :style="{backgroundColor: character.color }" :class="{ selected: active(character)}" v-for="character in characters" :key=character.id @click=selectCharacter(character)>{{character.name}}</button>
   </div>
-  <form @submit.prevent="addProject">
-      <input type="text" v-model="projectName">
-      <swatches-picker v-model="color"/>
-      <button type="submit">Add a Project</button>
+  <form @submit.prevent="addCharacter">
+      <input type="text" v-model="characterName" placeholder="Name">
+      <input type="text" v-model="characterClass" placeholder="Class">
+      <input type="text" v-model="characterRace" placeholder="Race">
+      <!-- <swatches-picker v-model="color"/> -->
+      <button type="submit">Add a Character</button>
     </form>
-  <div class="todoItems" v-if="project">
-    <p v-show="activeItems.length === 0">You are done with all your tasks! Good job!</p>
-    <form @submit.prevent="addItem">
+  <div class="todoQuests" v-if="character">
+    <p v-show="activeQuests.length === 0">You are done with all your quests! Good job!</p>
+    <form @submit.prevent="addQuest">
       <input type="text" v-model="text">
-      <button type="submit">Add an Item</button>
+      <button type="submit">Add a Quest</button>
     </form>
     <div class="controls">
       <button @click="showAll()">Show All</button>
@@ -22,13 +24,15 @@
       <button @click="deleteCompleted()">Delete Completed</button>
     </div>
     <ul>
-      <li v-for="item in filteredItems" :key="item.id">
-        <label :class="{ item: true, completed: item.completed }">
-          {{ item.text }}
-          <input type="checkbox" v-model="item.completed" @click="completeItem(item)" />
+      <li v-for="quest in filteredQuests" :key="quest.id">
+        <label :class="{ quest: true, completed: quest.completed }">
+          {{ quest.title }}
+          {{ quest.description }}
+          {{ quest.reward }}
+          <input type="checkbox" v-model="quest.completed" @click="completeQuest(quest)" />
           <span class="checkmark"></span>
         </label>
-        <button @click="deleteItem(item)" class="delete">X</button>
+        <button @click="deleteQuest(quest)" class="delete">X</button>
       </li>
     </ul>
   </div>
@@ -37,103 +41,113 @@
 
 <script>
 import axios from 'axios';
-import { Swatches } from 'vue-color'
+// import { Swatches } from 'vue-color'
 export default {
   name: 'Home',
   components: {
-    'swatches-picker': Swatches
+    // 'swatches-picker': Swatches
   },
   data() {
     return {
-      projects: [],
-      project: null,
-      projectName: '',
-      color: "#000",
-      items: [],
-      text: '',
+      characters: [],
+      character: null,
+      characterName: '',
+      characterClass: '',
+      characterRace: '',
+      color: "#0F0",
+      quests: [],
+      title: '',
+      description: '',
+      reward: '',
       show: 'all',
     }
   },
   created() {
-    this.getProjects();
+    this.getCharacters();
   },
   computed: {
-    activeItems() {
-      return this.items.filter(item => {
-        return !item.completed;
+    activeQuests() {
+      return this.quests.filter(quest => {
+        return !quest.completed;
       });
     },
-    filteredItems() {
+    filteredQuests() {
       if (this.show === 'active')
-        return this.items.filter(item => {
-          return !item.completed;
+        return this.quests.filter(quest => {
+          return !quest.completed;
         });
       if (this.show === 'completed')
-        return this.items.filter(item => {
-          return item.completed;
+        return this.quests.filter(quest => {
+          return quest.completed;
         });
-      return this.items;
+      return this.quests;
     },
   },
   methods: {
-    async addProject() {
+    async addCharacter() {
       try {
-        await axios.post("/api/projects", {
-          name: this.projectName,
-          color: this.color.hex,
+        await axios.post("/api/characters", {
+          name: this.characterName,
+          class: this.characterClass,
+          race: this.characterRace,
         });
-        await this.getProjects();
+        await this.getCharacters();
       } catch (error) {
         console.log(error);
       }
     },
-    async getProjects() {
+    async getCharacters() {
       try {
-        const response = await axios.get("/api/projects");
-        this.projects = response.data;
+        const response = await axios.get("/api/characters");
+        console.log(response);
+        this.characters = response.data;
       } catch (error) {
         console.log(error);
       }
     },
-    selectProject(project) {
-      this.project = project;
-      this.getItems();
+    selectCharacter(character) {
+      this.character = character;
+      this.getQuests();
     },
-    async getItems() {
+    async getQuests() {
       try {
-        const response = await axios.get(`/api/projects/${this.project._id}/items`);
-        this.items = response.data;
+        const response = await axios.get(`/api/characters/${this.character._id}/quests`);
+        this.quests = response.data;
       } catch (error) {
         console.log(error);
       }
     },
-    async addItem() {
+    async addQuest() {
       try {
-        await axios.post(`/api/projects/${this.project._id}/items`, {
-          text: this.text,
+        await axios.post(`/api/characters/${this.character._id}/quests`, {
+          title: this.title,
+          description: this.description,
+          reward: this.reward,
           completed: false
         });
         this.text = "";
-        this.getItems();
+        this.getQuests();
       } catch (error) {
         console.log(error);
       }
     },
-    async completeItem(item) {
+    async completeQuest(quest) {
       try {
-        axios.put(`/api/projects/${this.project._id}/items/${item._id}`, {
-          text: item.text,
-          completed: !item.completed,
+        axios.put(`/api/characters/${this.character._id}/quests/${quest._id}`, {
+          title: this.title,
+          description: this.description,
+          reward: this.reward,
+          completed: !quest.completed,
         });
-        this.getItems();
+        this.getQuests();
       } catch (error) {
         console.log(error);
       }
     },
-    async deleteItem(item) {
+    async deleteQuest(quest) {
       try {
-        await axios.delete(`/api/projects/${this.project._id}/items/${item._id}`);
-        this.getItems();
+        await axios.delete(`/api/characters/${this.character._id}/quests/${quest._id}`);
+        this.getQuests();
       } catch (error) {
         console.log(error);
       }
@@ -148,15 +162,15 @@ export default {
       this.show = 'completed';
     },
     deleteCompleted() {
-      this.items.forEach(item => {
-        if (item.completed)
-          this.deleteItem(item);
+      this.quests.forEach(quest => {
+        if (quest.completed)
+          this.deleteQuest(quest);
       });
     },
-    active(project) {
-      return (this.project && project._id === this.project._id);
+    active(character) {
+      return (this.character && character._id === this.character._id);
     },
-    darkColor(color) {
+    /*darkColor(color) {
       return (this.lightOrDark(color) === 'dark');
     },
     lightOrDark(color) {
@@ -188,7 +202,7 @@ export default {
 
           return 'dark';
       }
-    }
+    }*/
   }
 }
 </script>
@@ -241,7 +255,7 @@ input[type=text] {
   margin-bottom: 20px;
 }
 
-#projects {
+#characters {
   margin-bottom: 20px;
 }
 
@@ -274,7 +288,7 @@ button.selected {
 
 /* Custom checkbox
 /* Customize the label (the container) */
-.item {
+.quest {
   display: block;
   position: relative;
   padding-left: 35px;
@@ -288,7 +302,7 @@ button.selected {
 }
 
 /* Hide the browser's default checkbox */
-.item input {
+.quest input {
   position: absolute;
   opacity: 0;
   cursor: pointer;
@@ -307,12 +321,12 @@ button.selected {
 }
 
 /* On mouse-over, add a grey background color */
-.item:hover input~.checkmark {
+.quest:hover input~.checkmark {
   background-color: #ccc;
 }
 
 /* When the checkbox is checked, add a blue background */
-.item input:checked~.checkmark {
+.quest input:checked~.checkmark {
   background-color: #2196F3;
 }
 
@@ -324,12 +338,12 @@ button.selected {
 }
 
 /* Show the checkmark when checked */
-.item input:checked~.checkmark:after {
+.quest input:checked~.checkmark:after {
   display: block;
 }
 
 /* Style the checkmark/indicator */
-.item .checkmark:after {
+.quest .checkmark:after {
   left: 9px;
   top: 5px;
   width: 5px;
